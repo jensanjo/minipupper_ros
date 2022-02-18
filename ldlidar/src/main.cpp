77d67394942beaa6f0a59c9b219ef791e69092d3
Joep Jansen <joep.w.jansen@gmail.com>
2022-02-18 16:03:09 +0100

    High CPU load fix https://github.com/AlessioMorale/ld06_lidar/issues/18

--- ldlidar/src/main.cpp
+++ ldlidar/src/main.cpp
@@ -48,28 +48,15 @@ int main(int argc , char **argv)
 	
 	ros::Publisher lidar_pub = nh.advertise<sensor_msgs::LaserScan>("/scan", 1); /*create a ROS topic */
 	
+	ros::Rate rate(30);
 	while (ros::ok())
 	{
 		if (lidar->IsFrameReady())
 		{
 			lidar_pub.publish(lidar->GetLaserScan());  // Fixed Frame:  lidar_frame
 			lidar->ResetFrameReady();
-#if 0 
-			sensor_msgs::LaserScan data = lidar->GetLaserScan();
-			unsigned int lens = (data.angle_max - data.angle_min) / data.angle_increment;  
-			std::cout << "current_speed: " << lidar->GetSpeed() << " " 
-			          << "len: " << lens << " "
-					  << "angle_min: " << RADIAN_TO_ANGLED(data.angle_min) << " "
-					  << "angle_max: " << RADIAN_TO_ANGLED(data.angle_max) << std::endl; 
-			std::cout << "----------------------------" << std::endl;
-			for (int i = 0; i < lens; i++)
-			{
-				std::cout << "range: " <<  data.ranges[i] << " " 
-						  << "intensites: " <<  data.intensities[i] << std::endl;
-			}
-			std::cout << "----------------------------" << std::endl;
-#endif
 		}
+		rate.sleep();
 	}
     return 0;
 }
